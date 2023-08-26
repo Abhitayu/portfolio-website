85a23b607ac09a8635ad50dde334a46247ca0021
Abhitayu <abhitayusharma42@gmail.com>
2023-08-26 11:18:13 +0530

    Secret key added to env

--- portfolio_website/portfolio_website/settings.py
+++ portfolio_website/portfolio_website/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7q7g&5_fa+*2p^hsxh44i@b6j(z#^^oql6h!%l5-2-sbstaw=^'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
