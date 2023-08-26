210cf6a85959b1dd978ccf4efe5f8e2ea9917177
Abhitayu <abhitayu.sharma@kpipartners.com>
2023-08-26 10:56:13 +0530

    commit

--- portfolio_website/home/views.py
+++ portfolio_website/home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,HttpResponse
-
+from datetime import datetime
+from home.models import Contact
+from django.core.mail import send_mail
 # Create your views here.
 def index(request):
     return render(request,'index.html')
@@ -8,4 +10,17 @@ def about(request):
     return render(request,'about.html')
 
 def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        desc = request.POST.get('desc')
+        send_mail(
+                'New Contact Form Submission',
+                f'Name: {name}\nEmail: {email}\nMessage: {desc}',
+                'abhitayusharma42@gmail.com',  # Replace with your email address
+                ['abhitayusharma42@gmail.com'],  # Replace with the recipient email address(es)
+                fail_silently=False,
+        )
+        contact = Contact(name=name,email=email,desc=desc,date=datetime.today())
+        contact.save()
     return render(request,'contact.html')
--- portfolio_website/portfolio_website/settings.py
+++ portfolio_website/portfolio_website/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'home.apps.HomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -115,7 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -127,3 +130,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
